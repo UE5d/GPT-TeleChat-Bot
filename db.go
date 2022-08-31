b588ce3469da5fd988c0755241f1c370d8193f0f
UE5d <113224074+UE5d@users.noreply.github.com>
2022-08-31 17:36:47 +0000

    Add some const

--- db.go
+++ db.go
@@ -22,4 +22,8 @@ type Message struct {
 
 	PromptTokens     int `json:"prompt_tokens,omitempty"`
 	CompletionTokens int `json:"completion_tokens,omitempty"`
-	TotalTokens      int `json:"total_tokens,omitempty
+	TotalTokens      int `json:"total_tokens,omitempty"`
+}
+
+// ConnectDB
+func
