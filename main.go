d112cd904272d8408417b3072bd56b3b78da0039
UE5d <113224074+UE5d@users.noreply.github.com>
2023-04-21 08:57:25 +0000

    Added message

--- main.go
+++ main.go
@@ -1,2 +1,26 @@
 
 package main
+
+import (
+	"context"
+	"math/rand"
+	"os"
+	"os/signal"
+	"strconv"
+	"time"
+
+	"github.com/go-telegram/bot"
+	"github.com/go-telegram/bot/models"
+	"github.com/joho/godotenv"
+	"github.com/rakyll/openai-go"
+	"github.com/rakyll/openai-go/chat"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+)
+
+var (
+	retainHistory bool
+	promptName    = "prompt.txt"
+)
+
+func main() {
